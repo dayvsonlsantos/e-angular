9ba0a0dc9668c1b2f79ad186e3a509726c99a65a
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-10-30 21:21:21 -0300

    feat: Learning Angular - Another example for the Display Task

--- angular_02/src/app/components/display/display.component.ts
+++ angular_02/src/app/components/display/display.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class DisplayComponent {
 
   showPass:boolean = false;
+  showPass2: boolean = false;
 
   values:number[] = [
     1,
@@ -16,12 +17,21 @@ export class DisplayComponent {
     4
   ]
 
+  timestamp:Date[] = [
+
+  ]
+
   value:number = this.values.length;
 
   showPassword(){
     this.showPass = !this.showPass
     this.value++
     return this.values.push(this.value)
+  }
+
+  showPassword2(){
+    this.showPass = !this.showPass
+    return this.timestamp.push(new Date())
   }
 
 }
